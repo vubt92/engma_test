ecc41a1ceab559766a845b22b83f6c5b8e8de5ed
Salamander <tranvubt92@gmail.com>
2017-04-10 00:21:57 +0700

    - add news

--- application/controllers/admin/news.php
+++ application/controllers/admin/news.php
@@ -15,5 +15,34 @@ Class News extends MY_Controller
         $data['temp'] = 'admin/news' ;
         $this->load->view('admin/news/index',$data);
     }
+
+    function add()
+    {
+        $this->load->library('form_validation');
+        $this->load->model('category_model');
+        $ctg = $this->category_model->get_list();
+        $data = array();
+        $data['temp'] = 'admin/news';
+        $data['ctg'] = $ctg;
+
+        if($this->input->post())
+        {
+            $this->form_validation->set_rules('title',"Tiêu đề bài viết",'required');
+            $this->form_validation->set_rules('content',"Nội dung bài viết",'required');
+            //$this->form_validation->set_rules('img',"Ảnh tiêu đề",'required');
+
+            if($this->form_validation->run())
+            {
+                $title = $this->input->post('title');
+                $content = $this->input->post('content');
+
+                $news_data = array(
+                    'title'=>$title,'content'=>$content
+                );
+                $this->news_model->insert($news_data);
+            }
+        }
+        $this->load->view('admin/news/add',$data);
+    }
 }
 ?>
